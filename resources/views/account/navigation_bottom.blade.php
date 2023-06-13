11f4b9fffa1fc0b8da168f4339c480606c73702d
hasnae <hasnaemadad@gmail.com>
2023-06-13 19:48:30 +0100

    first commit

---
+++ resources/views/account/navigation_bottom.blade.php
@@ -0,0 +1,5 @@
+<div class="footer">
+	<div class="container">
+		<b class="copyright">Library Management System </br> All rights reserved.
+	</div>
+</div>
---
+++ resources/views/account/style.blade.php
@@ -0,0 +1,22 @@
+<style>
+	.wrapper{
+		background-image: url({{  asset("images/abstrait-flou-centre-commercial-defocused.jpg")}});
+		background-color: #cccccc;
+		height: 500px;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		position: relative;
+	}
+
+    .wrapper1{
+		background-image: url({{ asset('images/vue-dessus-livres-cahier.jpg') }});
+		background-color: #cccccc;
+		height: 500px;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		position: relative;
+	}
+
+</style>
